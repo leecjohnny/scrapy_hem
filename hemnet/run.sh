29c1307c9f817eebb8b7bb68b88204d6101c9420
leecjohnny <johnny-lee1255@users.noreply.github.com>
2022-03-05 23:26:20 -0500

    modified run script

--- hemnet/run.sh
+++ hemnet/run.sh
@@ -1,9 +1,17 @@
-#!/bin/bash
+#!/bin/bash -e
 
-SCRAPY_OUPUT="data/items_$(date -u +"%Y%m%d_%H%_MZ").json"
-LOG_OUTPUT="logs/log_$(date -u +"%Y%m%d_%H%_MZ").txt"
-FILE_OUTPUT="data/listings_$(date -u +"%Y%m%d_%H%_MZ").txt"
+SCRAPY_OUPUT="data/items_$(date -u +"%Y%m%d_%H_%MZ").json"
+LOG_OUTPUT="logs/log_$(date -u +"%Y%m%d_%H_%MZ").txt"
+FILE_OUTPUT="data/listings_$(date -u +"%Y%m%d_%H_%MZ").txt"
 
 cd "$(dirname "$0")"
+source run.config
 scrapy crawl search -O $SCRAPY_OUPUT -L INFO --logfile $LOG_OUTPUT
 python3 process_scrapped_items.py $SCRAPY_OUPUT $FILE_OUTPUT
+
+curl -s --user $API_KEY \
+    https://api.mailgun.net/v3/mg.johnnyclee.com/messages \
+    -F from='Pi <pi@johnnyclee.com>' \
+    -F to=$TO_EMAIL \
+    -F subject='[Scrapy Status] Complete' \
+    -F text='($wc -l $FILE_OUTPUT) listings retrieved.'
